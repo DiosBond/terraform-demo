c775831e8e0d15e083ec3baa3109489ab3fabc33
dios <dios1986@ukr.net>
2024-05-23 15:40:06 +0300

    fix

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "kind_cluster" {
   source = "github.com/den-vasyliev/tf-kind-cluster"
 }
 module "flux_bootstrap" {
-  source            = "modules/fluxcd-flux-bootstrap"
+  source            = "https://github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
   config_path       = module.gke_cluster.kubeconfig
