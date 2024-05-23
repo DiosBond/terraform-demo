9c721c707d9cf37c6306bb0f9e5bc1828664fb72
dios <dios1986@ukr.net>
2024-05-23 15:42:43 +0300

    remove flux-bootstrap

--- main.tf
+++ main.tf
@@ -24,13 +24,7 @@ resource "kind_cluster" "this" {
 module "kind_cluster" {
   source = "github.com/den-vasyliev/tf-kind-cluster"
 }
-module "flux_bootstrap" {
-  source            = "https://github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
-  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
-  private_key       = module.tls_private_key.private_key_pem
-  config_path       = module.gke_cluster.kubeconfig
-  github_token      = var.GITHUB_TOKEN
-}
+
 module "github_repository" {
   source                   = "github.com/den-vasyliev/tf-github-repository"
   github_owner             = var.GITHUB_OWNER
