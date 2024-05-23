a43fdee8a078336eae063626a97bda1d38eae52f
dios <dios1986@ukr.net>
2024-05-23 14:43:16 +0300

    change main.tf for task 7.3

--- main.tf
+++ main.tf
@@ -4,3 +4,36 @@ module "gke_cluster" {
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
 }
+module "flux_bootstrap" {
+  source            = "./modules/fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.gke_cluster.kubeconfig
+  github_token      = var.GITHUB_TOKEN
+}
+module "github_repository" {
+  source                   = "github.com/DiosBond/new-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux"
+}
+module "tls_private_key" {
+  source = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
+}
+
+module "tls_private_key" {
+  source = "https://github.com/den-vasyliev/tf-hashicorp-tls-keys"
+
+  algorithm   = var.algorithm
+  ecdsa_curve = var.ecdsa_curve
+}
+
+output "private_key_pem" {
+  value = module.tls_private_key.private_key_pem
+}
+
+output "public_key_openssh" {
+  value = module.tls_private_key.public_key_openssh
+}
