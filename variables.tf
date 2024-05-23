aef3c7c333e01a68bfbf29b5ca15d3ce99c2d293
dios <dios1986@ukr.net>
2024-05-23 15:04:05 +0300

    add github_owner

--- variables.tf
+++ variables.tf
@@ -14,3 +14,15 @@ variable "GKE_NUM_NODES" {
   default     = 2
   description = "GKE nodes number"
 }
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  default     = 2
+  description = "GKE nodes number"
+}
+
+variable "GITHUB_OWNER" {
+  type        = string
+  default     = DiosBond
+  description = "GITHUB_OWNER"
+}
