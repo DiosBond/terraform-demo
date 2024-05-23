4fad25a5135dcb680fb976cf714bfba19cc2eb02
dios <dios1986@ukr.net>
2024-05-23 14:58:47 +0300

    add terraform.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+provider "kind" {
+  provider   = "docker"
+  kubeconfig = pathexpand("${path.module}/kind-config")
+}
+
+resource "kind_cluster" "this" {
+  name = "kind-cluster"
+  config = <<-EOF
+        apiVersion: kind.x-k8s.io/v1alpha4
+        kind: Cluster
+        nodes:
+        - role: control-plane
+        - role: worker
+    EOF
+}
+
 
 module "kind_cluster" {
   source = "github.com/den-vasyliev/tf-kind-cluster"
